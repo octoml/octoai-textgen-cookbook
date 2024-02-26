3d8be1a9a7ad50d88b2e24267a285865fc3039c2
Thierry Moreau <tmoreau@octoml.ai>
2024-02-26 14:26:07 -0500

    ask technically page

--- doctalk/frontend/src/app/page.tsx
+++ doctalk/frontend/src/app/page.tsx
@@ -5,27 +5,17 @@ export default function Home() {
     <main className="container flex-height">
       <div className="hero">
         <h1>
-          Create your own
-          <span style={{ display: "block" }}>RAG chat application</span>
+          <span style={{ display: "block" }}>AskTechnically</span>
         </h1>
       </div>
       <GeneratedImage />
       <div>
         <p className="credit">
-          DocTalk is a RAG chat app built using Python + NextJS that can search,
-          summarize, and answer questions from a set of docs. This demo is
-          utilizing documentation from{" "}
-          <a href="https://octo.ai/?utm_source=doctalk" target="_blank">
-            OctoAI
-          </a>{" "}
-          and{" "}
-          <a href="https://www.pinecone.io/" target="_blank">
-            Pinecone.
+          AskTechnically is a RAG chat app built using Python + NextJS that can search and answer questions from a set of docs. This demo is
+          utilizing articles from{" "}
+          <a href="https://technically.dev/" target="_blank">
+            technically.dev.
           </a>
-        </p>
-        <p className="disclaimer">
-          Please evaluate model response quality independently before using
-          these for production use cases.
         </p>
       </div>
     </main>
