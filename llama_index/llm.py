4d51b0e26901ee048c1d4a068c72d25c1fe891dc
Pedro Torruella <5025399+ptorru@users.noreply.github.com>
2024-04-25 08:26:55 -0700

    added print function

--- llama_index/llm.py
+++ llama_index/llm.py
@@ -4,3 +4,4 @@ from os import environ
 OCTOAI_API_KEY = environ.get("OCTOAI_TOKEN")
 octoai = OctoAI(model="llama-2-13b-chat", token=OCTOAI_API_KEY)
 response = octoai.complete("Octopi can not play chess because...")
+print(response)
