c5915259d9b945658c266bd4c5c59e3d49895488
Pedro Torruella <5025399+ptorru@users.noreply.github.com>
2024-04-25 08:30:41 -0700

    adding chat message version

--- llama_index/llm.py
+++ llama_index/llm.py
@@ -2,6 +2,24 @@ from llama_index.llms.octoai import OctoAI
 from os import environ
 
 OCTOAI_API_KEY = environ.get("OCTOAI_TOKEN")
+
 octoai = OctoAI(model="llama-2-13b-chat", token=OCTOAI_API_KEY)
+
+# Using complete
 response = octoai.complete("Octopi can not play chess because...")
+print(response)
+
+print("\n=====================\n")
+
+# Using the chat interface
+from llama_index.core.llms import ChatMessage
+
+messages = [
+    ChatMessage(
+        role="system",
+        content="Below is an instruction that describes a task. Write a response that appropriately completes the request.",
+    ),
+    ChatMessage(role="user", content="Write a short blog about Seattle"),
+]
+response = octoai.chat(messages)
 print(response)
