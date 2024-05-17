b48985c9854c33a139530e6bb1c45a6407caa5a8
Thierry Moreau <tmoreau@octoml.ai>
2024-05-17 14:32:35 -0400

    update doctalk implementation with pinecone attribution tags, using pinecone serverless, and latest Langchain and OctoAI SDKs

--- doctalk/vector_db/list_urls.py
+++ doctalk/vector_db/list_urls.py
@@ -1,46 +1,60 @@
+import json
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urlparse, urljoin
+from urllib.parse import urljoin
 
 visited_links = set()
-
-
-def to_json():
-    input_file = "data/pinecone_docs_urls.json"  # The name of your input file
-    output_file = "pinecone_docs_urls_output.txt"  # The name of your output file
-
-    with open(input_file, "r") as infile, open(output_file, "w") as outfile:
-        for line in infile:
-            url = line.strip()  # Remove any leading/trailing whitespace
-            formatted_line = '{{"url": "{}"}},\n'.format(url)
-            outfile.write(formatted_line)
-
-    print("File has been converted.")
-
 
 def scrape_links(url):
     if url in visited_links:
         return
+
     print("Scraping:", url)
-    visited_links.add(url)
+    try:
+        visited_links.add(url)
 
-    response = requests.get(url)
+        response = requests.get(url)
 
-    if response.status_code != 200:
-        print(f"Failed to retrieve the page: {url}")
+        if response.status_code != 200:
+            print(f"Failed to retrieve the page: {url}")
+            return
+
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        for link in soup.find_all("a"):
+            href = link.get("href")
+            if href:
+                absolute_url = urljoin(url, href)
+                if absolute_url.startswith(base_url):
+                    if "#" not in absolute_url:
+                        scrape_links(absolute_url)
+    except:
+        print("Something went wrong...")
         return
-
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    for link in soup.find_all("a"):
-        href = link.get("href")
-        if href:
-            absolute_url = urljoin(url, href)
-            if base_url in absolute_url:
-                scrape_links(absolute_url)
 
 
 if __name__ == "__main__":
-    # to_json()
-    base_url = "https://docs.pinecone.io/docs"
-    scrape_links(base_url)
+    # Scrape links (base URL is used to avoid external links)
+    base_url = "https://docs.pinecone.io"
+    scrape_links("https://docs.pinecone.io/home")
+    # Prepare the dictionary
+    url_list = []
+    for url in visited_links:
+        url_list.append({"url": url})
+    # Dump to file
+    json_str = json.dumps(url_list, indent=4)
+    with open('data/pinecone_docs_urls.json', 'w') as fp:
+        print(json_str, file=fp)
+
+    # Scrape links (base URL is used to avoid external links)
+    visited_links = set()
+    base_url = "https://octo.ai"
+    scrape_links("https://octo.ai")
+    # Prepare the dictionary
+    url_list = []
+    for url in visited_links:
+        url_list.append({"url": url})
+    # Dump to file
+    json_str = json.dumps(url_list, indent=4)
+    with open('data/octoai_docs_urls.json', 'w') as fp:
+        print(json_str, file=fp)
