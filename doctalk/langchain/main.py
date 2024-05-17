55b1f5be4d37b28ea788e13041064770f1edc74a
Thierry Moreau <tmoreau@octoml.ai>
2024-05-17 14:40:06 -0400

    update doctalk implementation with pinecone attribution tags, using pinecone serverless, and latest Langchain and OctoAI SDKs (#47)

--- doctalk/langchain/main.py
+++ doctalk/langchain/main.py
@@ -1,126 +1,97 @@
 import os
 import json
 import time
-import pinecone
+from pinecone import Pinecone
+from langchain_pinecone import PineconeVectorStore
+from langchain.chains import RetrievalQAWithSourcesChain
 from langchain_community.llms.octoai_endpoint import OctoAIEndpoint
-from langchain.chains import ConversationalRetrievalChain
 from langchain_community.embeddings import OctoAIEmbeddings
-from langchain_community.vectorstores import Pinecone
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
-# Configuration Constants
-TRANSFORMERS_CACHE_DIR = "/tmp/transformers_cache"
+
 # Show the response time
-SHOW_RESP_TIME = False
+SHOW_RESP_TIME = True
 # Environment Variables
-PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY", "PINECONE_API_KEY")
+PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
 PINECONE_ENV = os.environ.get("PINECONE_ENV", "PINECONE_ENV")
 OCTOAI_TOKEN = os.environ.get("OCTOAI_TOKEN")
-OCTOAI_ENDPOINT_URL = os.environ.get("OCTOAI_ENDPOINT_URL")
-OCTOAI_MODEL = os.environ.get("OCTOAI_MODEL")
+OCTOAI_ENDPOINT_URL = os.environ.get("OCTOAI_ENDPOINT_URL", "https://text.octoai.run/v1")
+OCTOAI_MODEL = os.environ.get("OCTOAI_MODEL", "meta-llama-3-70b-instruct")
 
 if OCTOAI_TOKEN is None:
     raise ValueError("OCTOAI_TOKEN environment variable not set.")
+if PINECONE_API_KEY is None:
+    raise ValueError("PINECONE_API_KEY environment variable not set.")
+if PINECONE_ENV is None:
+    raise ValueError("PINECONE_ENV environment variable not set.")
 
-# Set cache directory for transformers
-os.environ["HF_HOME"] = TRANSFORMERS_CACHE_DIR
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def predict(pc, user_question="how to avoid cold starts?"):
 
-# Utility Functions
-def get_octo_embed_model():
-    return OctoAIEmbeddings(
+    # LLM init
+    llm = OctoAIEndpoint(
+        model="meta-llama-3-70b-instruct",
+        max_tokens=2048,
+        temperature=0.01
+    )
+
+    # Embedding init
+    embeddings = OctoAIEmbeddings(
         endpoint_url="https://text.octoai.run/v1/embeddings",
         octoai_api_token=OCTOAI_TOKEN,
     )
 
-
-def get_pinecone_vector_store(index_name):
-    text_field = "text"  # the metadata field that contains our text
-    embed_model = get_octo_embed_model()
-    index = pinecone.Index(index_name)
-    # initialize the vector store object
-    vectorstore = Pinecone(index, embed_model, text_field)
-
-    return vectorstore
-
-
-def get_language_models():
-    return OctoAIEndpoint(
-        octoai_api_token=OCTOAI_TOKEN,
-        endpoint_url=OCTOAI_ENDPOINT_URL,
-        model_kwargs={
-            "messages": [
-                {
-                    "role": "system",
-                    "content": "Write a response that appropriately completes the request. Be clear and concise. Format your response as bullet points whenever possible.",
-                }
-            ],
-            "model": OCTOAI_MODEL,
-            "max_tokens": 1024,
-        },
-    )
-
-
-def execute_and_print(llm, retriever, question, model_name):
-    start_time = time.time()
-
-    try:
-        qa = ConversationalRetrievalChain.from_llm(
-            llm, retriever, max_tokens_limit=2000
-        )
-        response = qa.invoke({"question": question, "chat_history": []})
-    except Exception as e:
-        print(e)
-        exit(1)
-
-    end_time = time.time()
-    result = f"\n{model_name}\n"
-    result += response["answer"]
-
-    if SHOW_RESP_TIME == "True":
-        result += f"\n\nResponse ({round(end_time - start_time, 1)} sec)"
-
-    return result
-
-
-def predict(prompt="how to avoid cold starts?"):
-
-    try:
-        llm = get_language_models()
-    except Exception as e:
-        print("Error initializing language models")
-        print(e)
-        exit(1)
-
     # Get Pinecone vector store, the index_name should match step 1
     index_name = "doctalk"
-    vectorstore = get_pinecone_vector_store(index_name)
-
-    # Perform similarity search on top-5 answers
-    retriever = vectorstore.as_retriever(
-        search_type="similarity",
-        search_kwargs={"k": 5}
+    text_field = "text"  # the metadata field that contains our text
+    index = pc.Index(index_name)
+    vectorstore = PineconeVectorStore(
+        index, embeddings, text_field
     )
 
-    # Run the LLM RAG chain to answer the question
-    result = execute_and_print(llm, retriever, prompt, "#OctoAI LLM#")
+    # Retrieval Q&A chain with sources
+    # We do similarity search and return the top 5 results
+    qa = RetrievalQAWithSourcesChain.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=vectorstore.as_retriever(
+            search_type="similarity",
+            search_kwargs={"k": 5}
+        )
+    )
 
-    return result.strip("\n")  # Join and return combined results
+    # Time the RAG
+    start_time = time.time()
+    response = qa(user_question)
+    end_time = time.time()
+
+    # Prepare the return string
+    result = f"\n#OctoAI LLM#\n"
+    result += response["answer"]
+    result += "\n\nSources: {}".format(response["sources"])
+    if SHOW_RESP_TIME == True:
+        result += "\n\nResponse: {:.2f}s".format(end_time - start_time)
+
+    return result.strip("\n")
 
 
 # Main Handler Function for when the code is used as an AWS lambda function
 def handler(event, context):
-    pinecone.init(
-        api_key=PINECONE_API_KEY,
-        environment=PINECONE_ENV,
-    )
-    body = json.loads(event["body"])
-    prompt = body.get("prompt", None)
-    answer = predict(prompt)
-    return {"statusCode": 200, "body": json.dumps({"message_llm1": answer.strip("\n")})}
+    try:
+        # Init pinecone
+        pc = Pinecone(
+            api_key=PINECONE_API_KEY,
+            source_tag="octoai-doctalk"
+        )
+        # Run RAG and format the response accordingly
+        body = json.loads(event["body"])
+        prompt = body.get("prompt", None)
+        answer = predict(pc, prompt)
+        return {"statusCode": 200, "body": json.dumps({"message_llm1": answer.strip("\n")})}
+    except:
+        return {"statusCode": 500, "body": "Something went wrong running RAG on the AWS Lambda"}
 
 
 # CLI interaction
